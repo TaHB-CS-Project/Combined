e48d36f035296fea381f4e2c74157c3a0a303373
tabriscode <michael-540337@hotmail.com>
2021-04-11 00:56:20 -0500

    Revert "login from db from session.go"
    This reverts commit 4188414a6c6da77c2fbdfa13ab34b54f9a3e74e7.

--- session.go
+++ session.go
@@ -8,20 +8,6 @@ import (
 	"github.com/gorilla/sessions"
 )
 
-type user_entity struct {
-	// user_id               string
-	// medicalemployee_id    int
-	// email                 string
-	// email_confirmed       bool
-	// email_confirmed_token string
-	Username      string `json:email`
-	Password_hash string `json:password`
-	// salt                  string
-	// lockout               bool
-	// reset_password_stamp  string
-	// reset_password_date   string
-}
-
 var store = sessions.NewCookieStore([]byte("session"))
 
 func Index(response http.ResponseWriter, request *http.Request) {
@@ -38,21 +24,8 @@ func Login(response http.ResponseWriter, request *http.Request) {
 	password := request.Form.Get("password")
 	fmt.Println("email:", username)
 	fmt.Println("password:", password)
-
-	user := user_entity{}
-
-	sqlStatement := `
-	SELECT password_hash 
-	FROM user_entity 
-	WHERE username=$1`
-	result := db.QueryRow(sqlStatement, username)
-	err := result.Scan(&user.Password_hash)
-	if err != nil {
-		fmt.Println("User/Pass was invalid")
-	}
-
-	check := password == user.Password_hash
-	if check {
+	//if the password and username matches from the database
+	if username == "doctor" && password == "password" {
 		sessions, _ := store.Get(request, "session")
 		sessions.Values["username"] = username
 
@@ -61,15 +34,13 @@ func Login(response http.ResponseWriter, request *http.Request) {
 		//if their username and password matches then redirect them to the dashboard(?) or whatever is the
 		//main page of a succesful login
 		http.Redirect(response, request, "/dashboard", http.StatusSeeOther)
-	}
-	if !check { //username and password doesn't match
+	} else { //username and password doesn't match
 		data := map[string]interface{}{
 			"err": "Invalid Username or Password.",
 		}
 		tmp, _ := template.ParseFiles("Template/index.html")
 		tmp.Execute(response, data)
 	}
-
 }
 func Logout(response http.ResponseWriter, request *http.Request) {
 	//get the current session
