bf42e1a86a5297b0899e41a6671d7bf23b16cd70
hotduck95 <55869067+hotduck95@users.noreply.github.com>
2021-04-13 15:05:19 -0500

    add supervisor data

--- js/staff-list.js
+++ js/staff-list.js
@@ -31,8 +31,16 @@ $.getJSON("staff-list.json",
         info += '<td>' + 
             value.Medicalemployee_classification + '</td>';
             
-        info += '<td>' + 
-        value.Medicalemployee_supervisor + '</td>';
+        if(value.Medicalemployee_supervisor.Valid == false){
+            info += '<td> None </td>';  
+        }
+        else if(value.Medicalemployee_supervisor.Valid == true){
+            info += '<td>' +
+            value.Medicalemployee_supervisor.String + '</td>';
+        }
+        else{
+            info += '<td> Error </td>';
+        }
 
         info += '</tr>';
     
