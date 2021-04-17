1a0d7e74a8bc6ad6733ab434706a8b75c6f52abc
hotduck95 <55869067+hotduck95@users.noreply.github.com>
2021-04-17 01:40:26 -0500

    connect json file

--- js/add-record.js
+++ js/add-record.js
@@ -1,46 +1,25 @@
-/*$(document).ready(function(){
-    var maxField = 10; 
-    var add_diagnosis_button = $('.add_diagnosis_button'); 
-    var diagnosis_wrapper = $('.diagnosis_wrapper'); 
-    var diagnosisHTML = '<div style="display:flex;"><select name="diagnosis[]" class="form-control" id="diagnosis" style="margin-top:10px;"><option>Choose one</option><option>-----------</option><option>Intubation</option><option>UVC</option><option>UAC</option><option>PICC</option><option>Peritoneal tap</option><option>Urinary catheterization</option><option>Lumbar puncture</option><option>Ventricular tap</option><option>PIV</option><option>Chest tube placement</option></select><a href="javascript:void(0);" class="remove_diagnosis_button" title="Remove field"><i class="fas fa-minus-circle" style="font-size:22px; margin:15px 0 0 5px;"></i></a></div>'; 
-    var x = 1; //Initial field counter is 1
-    
 
-    $(add_diagnosis_button).click(function(){
-        if(x < maxField){ 
-            x++; 
-            $(diagnosis_wrapper).append(diagnosisHTML);
-        }
-    });
-    
-
-    $(diagnosis_wrapper).on('click', '.remove_diagnosis_button', function(e){
-        e.preventDefault();
-        $(this).parent('div').remove(); 
-        x--; 
+$.getJSON("js/hospitalnamelist.json",
+function (data) {
+    $.each(data,
+    function (key, value) {
+    $("#hospital").append("<option value='" + value.Hospital_name + "'>" + value.Hospital_name + "</option>");
     });
 });
 
-$(document).ready(function(){
-    var maxField = 10;
-    var add_procedure_button = $('.add_procedure_button');
-    var procedure_wrapper = $('.procedure_wrapper'); 
-    var procedureHTML = '<div style="display:flex;"><select name="procedure[]" class="form-control" id="procedure" style="margin-top:10px;"><option>Choose one</option><option>-----------</option><option>Intubation</option><option>UVC</option><option>UAC</option><option>PICC</option><option>Peritoneal tap</option><option>Urinary catheterization</option><option>Lumbar puncture</option><option>Ventricular tap</option><option>PIV</option><option>Chest tube placement</option></select><a href="javascript:void(0);" class="remove_procedure_button" title="Remove field"><i class="fas fa-minus-circle" style="font-size:22px; margin:15px 0 0 5px;"></i></a></div>';
-    var y = 1; 
-    
-
-    $(add_procedure_button).click(function(){
-        if(y < maxField){ 
-            y++; 
-            $(procedure_wrapper).append(procedureHTML); 
-        }
-    });
-    
-
-    $(procedure_wrapper).on('click', '.remove_procedure_button', function(e){
-        e.preventDefault();
-        $(this).parent('div').remove(); 
-        y--; 
+$.getJSON("js/diagnosis.json",
+function (data) {
+    $.each(data,
+    function (key, value) {
+    $("#diagnosis").append("<option value='" + value.Diagnosis_id + "'>" + value.Diagnosis_name + "</option>");
     });
 });
-*/
+
+$.getJSON("js/procedure.json",
+function (data) {
+    $.each(data,
+    function (key, value) {
+    $("#procedure").append("<option value='" + value.Procedure_id + "'>" + value.Procedure_name + "</option>");
+    });
+});
+
